a05f447d77024146bca0d35f0d80a25de1eacb69
Rita <ritanoblejane@gmail.com>
2025-06-05 11:23:35 +0100

    updated vite.config file

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Find-A-Country/",
+  base: "/NationSnap/",
   plugins: [react(), tailwindcss(),],
 })
